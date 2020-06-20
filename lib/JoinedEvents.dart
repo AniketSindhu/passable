4c91d0f0f209d9222601590483b8d58956a3b6ec
AniketSindhu <sindhuaniket@gmail.com>
2020-06-20 07:13:03 +0530

    joined events ui almost done

--- lib/JoinedEvents.dart
+++ lib/JoinedEvents.dart
@@ -3,9 +3,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:plan_it_on/config/size.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'config/config.dart';
 import 'googleSignIn.dart';
 import 'loginui.dart';
@@ -79,7 +79,69 @@ class _JoinedEventsState extends State<JoinedEvents> {
                   child: ListView.builder(
                   itemCount: snapshot.data.length,
                   itemBuilder:(context,index){
-                    return ListTile(title: Text("${snapshot.data[index].data['eventCode']}"),);
+                    return Padding(
+                      padding: const EdgeInsets.fromLTRB(0,0,0,20),
+                      child: Container(
+                        margin: EdgeInsets.symmetric(horizontal:width*0.02),
+                        height: height*0.3,
+                        child: Card(
+                          color: Colors.deepPurple[50],
+                          shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(10)),
+                          elevation: 4,
+                          child:Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children:<Widget>[
+                              Expanded(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Align(alignment:Alignment.topLeft,child: Image.network(snapshot.data[index].data['eventBanner'],fit: BoxFit.fitHeight,)),
+                                    Expanded(
+                                      child: Padding(
+                                        padding: const EdgeInsets.fromLTRB(8.0,16,8,0),
+                                        child: Column(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Align(
+                                              alignment:Alignment.topLeft,
+                                              child: Text("${snapshot.data[index].data['eventName']}",style:GoogleFonts.poppins(textStyle:TextStyle(fontWeight:FontWeight.w600,fontSize:20,color: AppColors.primary)),textAlign: TextAlign.start,)
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.fromLTRB(0,0,0,8.0),
+                                              child: Align(
+                                                alignment:Alignment.bottomLeft,
+                                                child: Column(
+                                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                                  children: [
+                                                    Text('${DateFormat('hh:mm a').format(snapshot.data[index].data['eventDateTime'].toDate())}',style: TextStyle(fontWeight:FontWeight.w600,fontSize: 18),),
+                                                    Text('${DateFormat('EEE, d MMMM').format(snapshot.data[index].data['eventDateTime'].toDate())}',style: TextStyle(fontWeight:FontWeight.w400,fontSize: 14),),
+                                                  ],
+                                                ),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Align(
+                                alignment:Alignment.bottomCenter,
+                                child:Container(
+                                  width: width,
+                                  child: Padding(
+                                    padding: const EdgeInsets.fromLTRB(10.0,10,10,5),
+                                    child: Text("${snapshot.data[index].data['eventAddress']}",style:TextStyle(color: Colors.white,fontSize:16,fontWeight: FontWeight.w500)),
+                                  ),
+                                  color: AppColors.secondary,
+                                )
+                              )
+                            ]
+                          ) ,
+                        ),
+                      ),
+                    );
                   }
                 ),
               );
